7f1d010d71bb3659249282500f4fa00a715cb3bb
Nderkovich <nikitaderkovich@gmail.com>
2020-01-29 18:10:49 +0300

    Admin site and template

--- game_app/admin.py
+++ game_app/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Profile
+from game_app.models import Profile, Game
 
 
 @admin.register(Profile)
 class ProfleAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
     pass
