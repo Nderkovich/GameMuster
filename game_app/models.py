3b32a1d4fd62ed8e45df0b033ac9fb6f276e0211
Nderkovich <nikitaderkovich@gmail.com>
2020-01-29 18:08:39 +0300

    Added new model

--- game_app/models.py
+++ game_app/models.py
@@ -4,3 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 class Profile(AbstractUser):
     birthday = models.DateField(null=True)
+
+
+class Game(models.Model):
+    game_id = models.IntegerField()
+    user_profiles = models.ManyToManyField(Profile, related_name='favorite_games', null=True)
