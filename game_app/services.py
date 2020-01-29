1e2f51c64d91517f6511ec41860bb72192d4ee28
Nderkovich <nikitaderkovich@gmail.com>
2020-01-29 14:06:49 +0300

    Views changes

---
+++ game_app/services.py
@@ -0,0 +1,26 @@
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes
+
+from .models import Profile
+from .tokens import TokenGenerator
+
+
+def create_confirm_token(user):
+    token_generator = TokenGenerator()
+    token = token_generator.make_token(user)
+    return token
+
+
+def send_activation_email(user, token, current_site):
+    mail_subject = 'Activate your account.'
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    activation_link = f'{current_site}/activate/uid={uid}/token={token}/'
+    message = f'Hello {user.first_name} {user.last_name},\n {activation_link}'
+    email = EmailMessage(mail_subject, message, to=[user.email])
+    email.send()
+
+
+def check_token(user, token):
+    token_generator = TokenGenerator()
+    return token_generator.check_token(user, token)
--- game_app/urls.py
+++ game_app/urls.py
@@ -6,12 +6,12 @@ app_name = 'games'
 
 
 urlpatterns = [
-    path('', views.game_list, name='main_page'),
-    path('page/<int:page>/', views.game_list, name='gamelist_page'),
-    path('game/<int:id>/', views.game_info, name='game_info'),
+    path('', views.game_list_view, name='main_page'),
+    path('page/<int:page>/', views.game_list_view, name='game_list_page'),
+    path('game/<int:game_id>/', views.game_info, name='game_info'),
     path('search/page/<int:page>/', views.SearchView.as_view(), name='search'),
     path('sign_in/', views.sign_in, name='sign_in'),
     path('sign_up/', views.sign_up, name='sign_up'),
-    path('profile/<int:id>', views.profile, name='profile'),
-    path('activate/uid=<str:uidb64>/token=<str:token>/', views.activate, name='activation')
+    path('profile/<int:id>', views.profile_view, name='profile'),
+    path('activate/uid=<str:uidb64>/token=<str:token>/', views.activation_view, name='activation')
 ]
