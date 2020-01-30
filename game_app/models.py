23d2a7652da7a2dc5e093f8c781f44140a23c87d
Nderkovich <nikitaderkovich@gmail.com>
2020-01-30 12:55:11 +0300

    Added helper function

--- game_app/models.py
+++ game_app/models.py
@@ -5,6 +5,11 @@ from django.contrib.auth.models import AbstractUser
 class Profile(AbstractUser):
     birthday = models.DateField(null=True)
 
+    def is_in_favorite(self, game_id):
+        if self.favorite_games.all().filter(game_id=game_id):
+            return True
+        return False
+
 
 class Game(models.Model):
     game_id = models.IntegerField()
