8d42bf6c782647ce020c4dbab1aef08b22b18dac
Nderkovich <nikitaderkovich@gmail.com>
2020-02-03 14:25:07 +0300

    Fixed game services

--- games/services.py
+++ games/services.py
@@ -5,9 +5,7 @@ from games.igdb_api import IGDBClient
 
 def get_user_favorite_games(user):
     api_client = IGDBClient(settings.IGDB_API_KEY, settings.IGDB_API_URL)
-    ids = []
-    for g in user.favorite_games.all():
-        ids.append(g.game_id)
+    ids = [game.game_id for game in user.favorite_games.all()]
     if ids:
         return api_client.get_games_by_ids(ids)
     else:
