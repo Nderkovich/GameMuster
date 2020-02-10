c5110b0ce2f277ec13df62732c3cf1fe2d1ba2b3
Nderkovich <nikitaderkovich@gmail.com>
2020-02-10 09:48:20 +0300

    Renamed 'game_fetcher'

--- games/tasks.py
+++ games/tasks.py
@@ -20,9 +20,9 @@ def game_fetch_task():
 
 @task()
 def game_getter_task(offset, limit):
-    game_fetcher = GameCreator()
+    game_creator = GameCreator()
     api_client = IGDBClient(settings.IGDB_API_KEY, settings.IGDB_API_URL)
     game_list = api_client.get_game_list_full_data(offset, limit)
     for game in game_list:
-        game_fetcher.create_game(game)
+        game_creator.create_game(game)
     logger.info(f"Game {offset} - {offset + limit}")
