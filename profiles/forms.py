c2e6cfaf09ef6ab8625333398f12f7ebc2547c0b
Nderkovich <nikitaderkovich@gmail.com>
2020-02-21 18:12:28 +0300

    Added form for edit view

--- profiles/forms.py
+++ profiles/forms.py
@@ -13,3 +13,9 @@ class SignUpForm(forms.Form):
     last_name = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+
+class ProfileInfoForm(forms.Form):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    birthday = forms.DateField()
