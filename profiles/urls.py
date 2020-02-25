42ad9bb843de1ea21fc7f51972b437b9e9e05c81
Nderkovich <nikitaderkovich@gmail.com>
2020-02-25 17:14:07 +0300

    Missing comma

--- profiles/urls.py
+++ profiles/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
     path('profile/<int:profile_id>', views.profile_view, name='profile'),
     path('activate/uid=<str:uidb64>/token=<str:token>/', views.activation_view, name='activation'),
-    path('edit_profile/', views.EditProfileView.as_view(), name='edit_profile')
+    path('edit_profile/', views.EditProfileView.as_view(), name='edit_profile'),
     path('logout/', views.logout_view, name='logout')
 ]
